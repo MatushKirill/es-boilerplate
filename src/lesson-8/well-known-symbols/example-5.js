14d4c31779a90ac0344c843bec9fbc9546e3d044
kmatushkin <kmatushkin@alteryx.com>
2017-11-29 12:21:25 +0200

    Matushkin Kirill's homework 8

--- src/lesson-8/well-known-symbols/example-5.js
+++ src/lesson-8/well-known-symbols/example-5.js
@@ -4,6 +4,18 @@ function Temperature(degrees) {
     this.degrees = degrees;
 }
 
+Temperature.prototype[Symbol.toPrimitive]  = function(hint)  {
+    if(hint === 'number'){
+        return this.degrees
+    }
+    if(hint === 'default'){
+        return `${this.degrees} degrees`
+    }
+    if(hint === 'string'){
+        return `${this.degrees}°`
+    }
+};
+
 const freezing = new Temperature(32);
 
 console.log(freezing + '!'); // [object Object]! → // 32 degrees!
